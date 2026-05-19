e2d691937b16eaec308215120103e62e3f409d08
Matias García <magarciam@fi.uba.ar>
2026-05-19 10:04:32 -0500

    fixes

--- titulo-tareas-myc/__manifest__.py
+++ titulo-tareas-myc/__manifest__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Project Task Title Auto',
-    'version': '16.0.1.0.0',
-    'summary': 'Rellena el título de la tarea desde el nombre de fantasía del cliente',
-    'author': 'Tu Nombre',
+    'name': 'Título de Tareas MYC',
+    'version': '18.0.1.0.0',
+    'summary': 'Rellena el título de la tarea con el nombre de fantasía del cliente',
+    'author': 'MatiGMS',
+    'category': 'Services/Project',
     'depends': ['project'],
     'data': [],
     'installable': True,
--- titulo-tareas-myc/models/project_task.py
+++ titulo-tareas-myc/models/project_task.py
@@ -8,21 +8,17 @@ class ProjectTask(models.Model):
 
     @api.model
     def default_get(self, fields_list):
-        # llamamos primero al default_get original
         res = super().default_get(fields_list)
 
-        # si ya viene   un name por contexto, no lo pisamos
         if res.get('name'):
             return res
 
-        # intentamos obtener el partner desde el contexto
         partner_id = self.env.context.get('default_partner_id')
         if not partner_id:
             return res
 
         partner = self.env['res.partner'].browse(partner_id)
 
-        # si el partner tiene x_studio_nombre_fantasa, lo usamos como título
         nombre_fantasia = getattr(partner, 'x_studio_nombre_fantasa', False)
         if nombre_fantasia:
             res['name'] = nombre_fantasia
