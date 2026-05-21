673ace8b1e56d9eb3d2665296f95de66d52f3db3
Matias García <magarciam@fi.uba.ar>
2026-05-21 08:14:16 -0500

    Update version from 16.0.1.0.0 to 19.0.1.0.0

--- titulo-tareas-myc/__manifest__.py
+++ titulo-tareas-myc/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Project Task Title Auto',
-    'version': '16.0.1.0.0',
+    'version': '19.0.1.0.0',
     'summary': 'Rellena el título de la tarea desde el nombre de fantasía del cliente',
     'author': 'Tu Nombre',
     'depends': ['project'],
