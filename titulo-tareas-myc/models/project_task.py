fd0c0e80960506965a09b235880782b35ae76583
Matias García <magarciam@fi.uba.ar>
2026-05-21 08:47:27 -0500

    simplify functions
    Simplificar funciones del módulo

--- titulo-tareas-myc/models/project_task.py
+++ titulo-tareas-myc/models/project_task.py
@@ -7,28 +7,11 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     @api.model
-    def default_get(self, fields_list):
-        res = super().default_get(fields_list)
-
-        if res.get('name'):
-            return res
-
-        partner_id = self.env.context.get('default_partner_id')
-        if not partner_id:
-            return res
-
-        partner = self.env['res.partner'].browse(partner_id)
-        nombre_fantasia = getattr(partner, 'x_studio_nombre_fantasa', False)
-        if nombre_fantasia:
-            res['name'] = nombre_fantasia
-
-        return res
-
-    @api.onchange('partner_id')
-    def _onchange_partner_id_set_name_from_fantasy(self):
-        if (
-            self.partner_id
-            and getattr(self.partner_id, 'x_studio_nombre_fantasa', False)
-            and not (self.name or '').strip()
-        ):
-            self.name = self.partner_id.x_studio_nombre_fantasa
+    def create(self, vals):
+        partner_id = vals.get('partner_id') or self.env.context.get('default_partner_id')
+        if partner_id:
+            partner = self.env['res.partner'].browse(partner_id)
+            nombre_fantasia = getattr(partner, 'x_studio_nombre_fantasa', False)
+            if nombre_fantasia:
+                vals['name'] = nombre_fantasia
+        return super().create(vals)
